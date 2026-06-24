Fix broadcast: rm [None,:] on dup_conns_off (already batched)

v2.6/v2_6/genome.py
@@ -66,7 +66,7 @@ def mutate(nodes, conns, key, innov_start, subst=0.1, ins=0.03, dele=0.02):
         dup_conns_off = jnp.where(jnp.isnan(dup_conns[:, 0])[:, None], jnp.nan, dup_conns)
         dup_conns_off = jnp.where(jnp.arange(CONN_PARAMS)[None, :] == 0, (innov + added).astype(jnp.float32), dup_conns_off)
         dup_conns_off = jnp.where(jnp.arange(CONN_PARAMS)[None, :] == 1, (na + do_ins.astype(jnp.int32)).astype(jnp.float32), dup_conns_off)
-        cc = jnp.where(do_dup & (jnp.arange(MAX_GENES) == (ca + do_ins.astype(jnp.int32)))[:, None], dup_conns_off[None, :], cc.astype(jnp.float32))
+        cc = jnp.where(do_dup & (jnp.arange(MAX_GENES) == (ca + do_ins.astype(jnp.int32)))[:, None], dup_conns_off, cc.astype(jnp.float32))
         added_dup = do_dup * 2
         n = n.at[i].set(nn); c = c.at[i].set(cc)
         return (n, c, innov + added.astype(jnp.int32) + added_dup.astype(jnp.int32)), None